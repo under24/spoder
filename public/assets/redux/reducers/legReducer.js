500031464d185c8660400562e8ce05593c027c2b
Constantine <50499405@mail.ru>
2017-07-14 23:36:27 +0300

    Updated misc reducer

--- public/assets/redux/reducers/legReducer.js
+++ public/assets/redux/reducers/legReducer.js
@@ -2,74 +2,58 @@
 
 let legReducerInitState = {
   1: {
-    // IDs
     legId: 1,
     coxaServoN: 1,
     femurServoN: 2,
     tibiaServoN: 3,
-    // side
     side: 'left',
     row: 'front'
   },
   2: {
-    // IDs
     legId: 2,
     coxaServoN: 4,
     femurServoN: 5,
     tibiaServoN: 6,
-    // side
     side: 'right',
     row: 'front'
   },
   3: {
-    // IDs
     legId: 3,
     coxaServoN: 7,
     femurServoN: 8,
     tibiaServoN: 9,
-    // side
     side: 'left',
     row: 'middle'
   },
   4: {
-    // IDs
     legId: 4,
     coxaServoN: 10,
     femurServoN: 11,
     tibiaServoN: 12,
-    // side
     side: 'right',
     row: 'middle'
   },
   5: {
-    // IDs
     legId: 5,
     coxaServoN: 13,
     femurServoN: 14,
     tibiaServoN: 15,
-    // side
     side: 'left',
     row: 'back'
   },
   6: {
-    // IDs
     legId: 6,
     coxaServoN: 16,
     femurServoN: 17,
     tibiaServoN: 18,
-    // side
     side: 'right',
     row: 'back'
   }
 }
 const legReducer = (state = legReducerInitState, action) => {
-  switch (action.type) {
-    // case "LEG_ANGLE_CHANGED":
-    //   return Object.assign(
-    //     {}, 
-    //     state,
-    //     { [action.legId]: Object.assign({}, state[action.legId], action.payload) }
-    //   );
-  }
+  // switch (action.type) {
+  //   case "a":
+  //     
+  // }
   return state;
 }
--- public/assets/redux/reducers/miscReducer.js
+++ public/assets/redux/reducers/miscReducer.js
@@ -1,7 +1,60 @@
 'use strict';
 
 let miscReducerInitState = {
-  
+  1: {
+    transverseDX: null,
+    transverseDY: null,
+    sagittalDX: null,
+    sagittalDY: null,
+    transverseDistance: null,
+    sagittalDistance: null,
+    sagittalAngle: null
+  },
+  2: {
+    transverseDX: null,
+    transverseDY: null,
+    sagittalDX: null,
+    sagittalDY: null,
+    transverseDistance: null,
+    sagittalDistance: null,
+    sagittalAngle: null
+  },
+  3: {
+    transverseDX: null,
+    transverseDY: null,
+    sagittalDX: null,
+    sagittalDY: null,
+    transverseDistance: null,
+    sagittalDistance: null,
+    sagittalAngle: null
+  },
+  4: {
+    transverseDX: null,
+    transverseDY: null,
+    sagittalDX: null,
+    sagittalDY: null,
+    transverseDistance: null,
+    sagittalDistance: null,
+    sagittalAngle: null
+  },
+  5: {
+    transverseDX: null,
+    transverseDY: null,
+    sagittalDX: null,
+    sagittalDY: null,
+    transverseDistance: null,
+    sagittalDistance: null,
+    sagittalAngle: null
+  },
+  6: {
+    transverseDX: null,
+    transverseDY: null,
+    sagittalDX: null,
+    sagittalDY: null,
+    transverseDistance: null,
+    sagittalDistance: null,
+    sagittalAngle: null
+  }
 }
 
 const miscReducer = (state = miscReducerInitState, action) => {
