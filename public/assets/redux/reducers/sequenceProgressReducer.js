22d418222d922b4962fb7f9a75e8f8572eca1dce
Constantine <50499405@mail.ru>
2017-08-14 21:01:23 +0300

    [Redux] Updated sequenceProgressReducer

--- public/assets/redux/reducers/sequenceProgressReducer.js
+++ public/assets/redux/reducers/sequenceProgressReducer.js
@@ -1,12 +1,12 @@
 'use strict';
 
-let sequenceProgressReducerInitState = null;
+let sequenceProgressReducerInitState = 0;
 
 const sequenceProgressReducer = (state = sequenceProgressReducerInitState, action) => {
   switch (action.type) {
-    case "a":
+    case "SEQUENCE_PROGRESS_PCT_CHANGED":
       {
-        
+        return action.payload;
       }
   }
   return state;
