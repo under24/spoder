382cca1a4f6d17dfdcb7a5840398d7d10844b6d4
Constantine <50499405@mail.ru>
2017-08-23 20:47:26 +0300

    Added transverseAnimationViewOffsetXY in viewOffsetsReducer

--- public/assets/redux/reducers/viewOffsetsReducer.js
+++ public/assets/redux/reducers/viewOffsetsReducer.js
@@ -6,43 +6,57 @@ let viewOffsetsReducerInitState = {
     transverseViewOffsetX: 0,
     sagittalViewOffsetY: 280,
     sagittalViewOffsetX: 0,
+    transverseAnimationViewOffsetX: 0,
+    transverseAnimationViewOffsetY: 0
   },
   2: {
     transverseViewOffsetY: 400,
     transverseViewOffsetX: 0,
     sagittalViewOffsetY: 280,
     sagittalViewOffsetX: 0,
+    transverseAnimationViewOffsetX: 0,
+    transverseAnimationViewOffsetY: 0
   },
   3: {
     transverseViewOffsetY: 250,
     transverseViewOffsetX: 0,
     sagittalViewOffsetY: 280,
     sagittalViewOffsetX: 0,
+    transverseAnimationViewOffsetX: 0,
+    transverseAnimationViewOffsetY: 0
   },
   4: {
     transverseViewOffsetY: 250,
     transverseViewOffsetX: 0,
     sagittalViewOffsetY: 280,
     sagittalViewOffsetX: 0,
+    transverseAnimationViewOffsetX: 0,
+    transverseAnimationViewOffsetY: 0
   },
   5: {
     transverseViewOffsetY: 100,
     transverseViewOffsetX: 0,
     sagittalViewOffsetY: 280,
     sagittalViewOffsetX: 0,
+    transverseAnimationViewOffsetX: 0,
+    transverseAnimationViewOffsetY: 0
   },
   6: {
     transverseViewOffsetY: 100,
     transverseViewOffsetX: 0,
     sagittalViewOffsetY: 280,
     sagittalViewOffsetX: 0,
+    transverseAnimationViewOffsetX: 0,
+    transverseAnimationViewOffsetY: 0
   }
 }
 
 const viewOffsetsReducer = (state = viewOffsetsReducerInitState, action) => {
   switch (action.type) {
-    case "a":
-      
+    case "VIEW_OFFSET_SHIFTED":
+      {
+        // return Object.assign({}, state, action.payload);
+      }
   }
   return state;
 }
