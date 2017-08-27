c4145e1cecf028fd2fe22c026bc6b811e8e6c3cb
Constantine <50499405@mail.ru>
2017-08-27 18:41:59 +0300

    [Minor] Update

--- public/assets/utils/BlueprintUtils.js
+++ public/assets/utils/BlueprintUtils.js
@@ -2,17 +2,28 @@
 
 // BU == BlueprintUtils
 let BU = {
-  getStartAndEndPctBasedOnLegId(legId) {
-    switch (legId) {
-      case 1:
-      case 6:
-        return { startPct: 50, endPct: 75 };
-      case 2:
-      case 5:
+  getStartAndEndPctBasedOnLegId(legId, gaits, type) {
+    let gaitIndex = this.getGaitIndexFromLegId(legId, gaits, type),
+        pctgs = this.mapGaitIndexToStartAndEndPct(gaitIndex);
+    
+    return pctgs;
+  },
+  getGaitIndexFromLegId(legId, gaits, type) {
+    for (let i = 0; i < gaits[type].length; i++) {
+      let result = gaits[type][i].indexOf(legId);
+      if (result !== -1) 
+        return i;
+    }
+  },
+  mapGaitIndexToStartAndEndPct(index) {
+    switch (index) {
+      case 0:
         return { startPct: 0, endPct: 25 };
-      case 3:
+      case 1:
         return { startPct: 25, endPct: 50 };
-      case 4:
+      case 2:
+        return { startPct: 50, endPct: 75 };
+      case 3:
         return { startPct: 75, endPct: 100 };
     }
   }
