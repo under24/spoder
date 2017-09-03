c0f5a1a1352deaeb0464186f8634e8ca974e443a
Constantine <50499405@mail.ru>
2017-09-03 20:07:23 +0300

    Added additional case for compatibility reasons

--- public/assets/utils/GenericUtils.js
+++ public/assets/utils/GenericUtils.js
@@ -5,25 +5,37 @@ let GU = {
   getSideFromLegId(legId) {
     switch (legId) {
       case 1:
+      case '1':
       case 3:
+      case '3':
       case 5:
+      case '5':
         return "left";
       case 2:
+      case '2':
       case 4:
+      case '4':
       case 6:
+      case '6':
         return "right";
     }
   },
   getRowFromLegId(legId) {
     switch (legId) {
       case 1:
+      case '1':
       case 2:
+      case '2':
         return "front";
       case 3:
+      case '3':
       case 4:
+      case '4':
         return "middle";
       case 5:
+      case '5':
       case 6:
+      case '6':
         return "back";
     }
   }
