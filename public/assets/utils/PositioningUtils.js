61601de5c5fd780720f0ad4ba698ef0f29389ed7
Constantine <50499405@mail.ru>
2017-09-03 15:33:42 +0300

    Temporarily commented out a function

--- public/assets/utils/PositioningUtils.js
+++ public/assets/utils/PositioningUtils.js
@@ -30,27 +30,27 @@ let PU = {
   getMovementCircleDiameter(transverseReachRadius, metaData) {
     return transverseReachRadius - metaData.impossibleRange;
   },
-  getMovementCenterCoords(metaData, coords, transverseReachRadius) {
-    // distance to the center of the circle + offset from impossible range
-    let distance = (transverseReachRadius + metaData.impossibleRange) / 2,
-        finalCoords;
-    
-    if (coords.offsetRotation === 0)
-      finalCoords = {
-        x: coords.transverseBaseX + distance,
-        y: coords.transverseBaseY
-      };
-    else {
-      // if side == left => use original offset, if side == right => flip offset number (1 => -1)
-      let angle;
-      switch (coords.side) {
-        case 'left':
-          angle = coords.offsetRotation; break;
-        case 'right':
-          angle = MU.flipNumber(coords.offsetRotation); break;
-      }
-      finalCoords = MU.getCoordsFromDistanceAndAngle(coords.transverseBaseX, coords.transverseBaseY, angle, distance);
-    }
-    return finalCoords;
-  }
+  // getMovementCenterCoords(metaData, coords, transverseReachRadius) {
+  //   // distance to the center of the circle + offset from impossible range
+  //   let distance = (transverseReachRadius + metaData.impossibleRange) / 2,
+  //       finalCoords;
+  //   
+  //   if (coords.offsetRotation === 0)
+  //     finalCoords = {
+  //       x: coords.transverseBaseX + distance,
+  //       y: coords.transverseBaseY
+  //     };
+  //   else {
+  //     // if side == left => use original offset, if side == right => flip offset number (1 => -1)
+  //     let angle;
+  //     switch (coords.side) {
+  //       case 'left':
+  //         angle = coords.offsetRotation; break;
+  //       case 'right':
+  //         angle = MU.flipNumber(coords.offsetRotation); break;
+  //     }
+  //     finalCoords = MU.getCoordsFromDistanceAndAngle(coords.transverseBaseX, coords.transverseBaseY, angle, distance);
+  //   }
+  //   return finalCoords;
+  // }
 }
