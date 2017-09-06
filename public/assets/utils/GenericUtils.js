cd4261df83c039840d1176cf68b2a80b5205cf79
Constantine <50499405@mail.ru>
2017-09-06 20:24:50 +0300

    Added GU.corerctAngle method and replaced some old methods with it

--- public/assets/utils/GenericUtils.js
+++ public/assets/utils/GenericUtils.js
@@ -38,5 +38,12 @@ let GU = {
       case '6':
         return "back";
     }
+  },
+  correctAngle(angle) {
+    if (angle === 360) angle = 0;
+    else if (angle > 360) angle -= 360;
+    else if (angle < 0) angle += 360;
+    
+    return angle
   }
 }
