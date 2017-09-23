72ac30259a727640c82125669ad3b350797d7410
Constantine <50499405@mail.ru>
2017-09-24 00:32:34 +0300

    [Redux] Brought back sequenceMetaDataReducer

--- public/assets/redux/initRedux.js
+++ public/assets/redux/initRedux.js
@@ -17,7 +17,7 @@ const movementReducer = combineReducers({
   // status: movementStatusReducer,
   // sequence: sequenceReducer,
   sequenceProgress: sequenceProgressReducer,
-  // sequenceMetaData: sequenceMetaDataReducer
+  sequenceMetaData: sequenceMetaDataReducer
 });
 
 const baseReducer = combineReducers({
