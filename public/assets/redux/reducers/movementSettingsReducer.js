68c9e6888e9a2684ad7c74e18c82ead647a98fe0
Constantine <50499405@mail.ru>
2017-09-24 15:21:34 +0300

    Updated movementSettingsReducer

--- public/assets/redux/reducers/movementSettingsReducer.js
+++ public/assets/redux/reducers/movementSettingsReducer.js
@@ -3,14 +3,27 @@
 let movementSettingsReducerInitState = {
   loop: true,
   applyDirectionAndTurnAngles: true,
-  movementGeneratorEnabled: false
+  movementGeneratorEnabled: false,
+  tps: 60,
+  iterationDuration: 750,
+  amountOfTicks: null // automaticcaly calculated from tps + iterationDuration
 }
+
+// init tps + iterationDuration into amountOfTicks
+movementSettingsReducerInitState.amountOfTicks = +(movementSettingsReducerInitState.iterationDuration / (1000 / movementSettingsReducerInitState.tps)).toFixed(0);
 
 const movementSettingsReducer = (state = movementSettingsReducerInitState, action) => {
   switch (action.type) {
     case "MOVEMENT_SETTINGS_CHANGED":
       {
-        return Object.assign({}, state, action.payload);
+        // if tps or iterationDuration in the payload then calc amountOfTicks
+        if (action.payload.tps || action.payload.iterationDuration) {
+          let tps = action.payload.tps || state.tps,
+              iterationDuration = action.payload.iterationDuration || state.iterationDuration;
+          return Object.assign({}, state, action.payload, { amountOfTicks: +(iterationDuration / (1000 / tps)).toFixed(0) })
+        }
+        else
+          return Object.assign({}, state, action.payload);
       }
   }
   return state;
