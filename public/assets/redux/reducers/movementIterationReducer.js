d1e2a1134293530b222118e3d1a34a55e6152516
Constantine <50499405@mail.ru>
2017-10-15 21:54:07 +0300

    [Redux] Moved amountOfTicks calculation out of the reducer to the component <movement-handler>

--- public/assets/redux/reducers/movementIterationReducer.js
+++ public/assets/redux/reducers/movementIterationReducer.js
@@ -11,23 +11,11 @@ let movementInterationReducerInitState = {
   blueprintDataSource: null
 }
 
-// init tps + duration into amountOfTicks
-movementInterationReducerInitState.amountOfTicks = +(movementInterationReducerInitState.duration / (1000 / movementInterationReducerInitState.tps)).toFixed(0);
-
 const movementInterationReducer = (state = movementInterationReducerInitState, action) => {
   switch (action.type) {
     case "MOVEMENT_ITERATION_VALUES_CHANGED":
       {
-        if (action.payload.tps || action.payload.duration) {
-          let tps = action.payload.tps || state.tps,
-              duration = action.payload.duration || state.duration;
-              
-          let amountOfTicks = +(duration / (1000 / tps)).toFixed(0);
-          
-          return Object.assign({}, state, action.payload, { amountOfTicks });
-        }
-        else
-          return Object.assign({}, state, action.payload);
+        return Object.assign({}, state, action.payload);
       }
     case "SEQUENCE_SHIFTED_XY_BATCHED":
       {
