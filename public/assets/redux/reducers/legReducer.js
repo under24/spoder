7b290d287af81962beca22fe4aff165e67cfaa46
Constantine <50499405@mail.ru>
2017-11-25 17:36:41 +0300

    [Minor] Renamed a few vars

--- public/assets/redux/reducers/legReducer.js
+++ public/assets/redux/reducers/legReducer.js
@@ -3,49 +3,49 @@
 let legReducerInitState = {
   1: {
     legId: 1,
-    coxaServoN: 1,
-    femurServoN: 2,
-    tibiaServoN: 3,
+    coxaServoPIN: 1,
+    femurServoPIN: 2,
+    tibiaServoPIN: 3,
     side: 'left',
     row: 'front'
   },
   2: {
     legId: 2,
-    coxaServoN: 4,
-    femurServoN: 5,
-    tibiaServoN: 6,
+    coxaServoPIN: 4,
+    femurServoPIN: 5,
+    tibiaServoPIN: 6,
     side: 'right',
     row: 'front'
   },
   3: {
     legId: 3,
-    coxaServoN: 7,
-    femurServoN: 8,
-    tibiaServoN: 9,
+    coxaServoPIN: 7,
+    femurServoPIN: 8,
+    tibiaServoPIN: 9,
     side: 'left',
     row: 'middle'
   },
   4: {
     legId: 4,
-    coxaServoN: 10,
-    femurServoN: 11,
-    tibiaServoN: 12,
+    coxaServoPIN: 10,
+    femurServoPIN: 11,
+    tibiaServoPIN: 12,
     side: 'right',
     row: 'middle'
   },
   5: {
     legId: 5,
-    coxaServoN: 13,
-    femurServoN: 14,
-    tibiaServoN: 15,
+    coxaServoPIN: 13,
+    femurServoPIN: 14,
+    tibiaServoPIN: 15,
     side: 'left',
     row: 'back'
   },
   6: {
     legId: 6,
-    coxaServoN: 16,
-    femurServoN: 17,
-    tibiaServoN: 18,
+    coxaServoPIN: 16,
+    femurServoPIN: 17,
+    tibiaServoPIN: 18,
     side: 'right',
     row: 'back'
   }
