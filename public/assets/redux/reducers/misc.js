d93e98b0cee088b7dae6748ce48f4efdcf7c2ad5
Constantine <50499405@mail.ru>
2017-12-24 00:18:00 +0300

    Removed obsolete state properties

--- public/assets/redux/reducers/misc.js
+++ public/assets/redux/reducers/misc.js
@@ -1,30 +1,12 @@
 'use strict';
 
 let miscReducerInitState = {
-  1: {
-    transverseDiff: null,
-    transverseDistance: null
-  },
-  2: {
-    transverseDiff: null,
-    transverseDistance: null
-  },
-  3: {
-    transverseDiff: null,
-    transverseDistance: null
-  },
-  4: {
-    transverseDiff: null,
-    transverseDistance: null
-  },
-  5: {
-    transverseDiff: null,
-    transverseDistance: null
-  },
-  6: {
-    transverseDiff: null,
-    transverseDistance: null
-  }
+  1: null,
+  2: null,
+  3: null,
+  4: null,
+  5: null,
+  6: null
 }
 
 const miscReducer = (state = miscReducerInitState, action) => {
