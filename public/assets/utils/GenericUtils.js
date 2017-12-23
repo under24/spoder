95017185c1ede89a7f121c767e99441b38d6051b
Constantine <50499405@mail.ru>
2017-12-23 22:23:22 +0300

    Added a method to filter in unique values and add them to the payload object

--- public/assets/utils/GenericUtils.js
+++ public/assets/utils/GenericUtils.js
@@ -47,5 +47,17 @@ let GU = {
     if (angle > 360 || angle < 0) angle = this.correctAngle(angle);
     
     return angle;
+  },
+  sieveOutAndGatherUp(source, target, payload, legId) {
+    // iterate source obj and compare it with that we have in the target obj
+    for (let key in source) {
+      // compare value with the corresponding one in the target obj
+      if (source[key] !== target[key]) {
+        // create an empty obj if it has not been created
+        payload[legId] = payload[legId] || {};
+        // add a unique value to the payload object
+        payload[legId][key] = source[key];
+      }
+    }
   }
 }
