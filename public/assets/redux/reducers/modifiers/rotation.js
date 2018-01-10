7a5f686b58d573febf3c3828367cd2d35cc183e2
Constantine <50499405@mail.ru>
2018-01-10 20:40:27 +0300

    Added 'normalize' functionality for rotation modifier

--- public/assets/redux/reducers/modifiers/rotation.js
+++ public/assets/redux/reducers/modifiers/rotation.js
@@ -2,6 +2,8 @@
 
 let rotationReducerInitState = {
   x: 0,
+  normalizer: 0.4, // x: 100 -> normalizedX: 40
+  normalizedX: 0,
   appliedTo: 'transverseBaseXY'
 }
 
@@ -9,7 +11,10 @@ const rotationReducer = (state = rotationReducerInitState, action) => {
   switch (action.type) {
     case "ROTATION_MODIFIER_CHANGED":
       {
-        return Object.assign({}, state, action.payload);
+        // calc normalized x (joystick value * normalizer)
+        let normalizedX = MU.normalize(action.payload.x, state.normalizer);
+        
+        return Object.assign({}, state, action.payload, { normalizedX });
       }
   }
   return state;
