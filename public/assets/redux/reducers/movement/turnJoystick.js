e668cfe7d6fc400ff10ee8087bc55fa14470ccc2
Constantine <50499405@mail.ru>
2018-01-10 20:51:19 +0300

    Added 'normalize' functionality for turn joystick

--- public/assets/redux/reducers/movement/turnJoystick.js
+++ public/assets/redux/reducers/movement/turnJoystick.js
@@ -1,12 +1,19 @@
 'use strict';
 
-let movementTurnJoystickReducerInitState = { x: 0 }
+let movementTurnJoystickReducerInitState = {
+  x: 0,
+  normalizer: 0.25, // x: 100 -> normalizedX: 25
+  normalizedX: 0
+}
 
 const movementTurnJoystickReducer = (state = movementTurnJoystickReducerInitState, action) => {
   switch (action.type) {
     case "MOVEMENT_TURN_JOYSTICK_VALUES_CHANGED":
       {
-        return Object.assign({}, state, action.payload);
+        // calc normalized x (joystick value * normalizer)
+        let normalizedX = MU.normalize(action.payload.x, state.normalizer);
+        
+        return Object.assign({}, state, action.payload, { normalizedX });
       }
   }
   return state;
