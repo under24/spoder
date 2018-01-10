dee5ace2b00df0647125134aea392c07cffcdaf4
Constantine <50499405@mail.ru>
2018-01-10 20:41:19 +0300

    Added 'normalize' functionality for shift modifier

--- public/assets/redux/reducers/modifiers/shift.js
+++ public/assets/redux/reducers/modifiers/shift.js
@@ -2,7 +2,10 @@
 
 let shiftReducerInitState = {
   x: 0,
+  normalizedX: 0,
   y: 0,
+  normalizedY: 0,
+  normalizer: 0.7, // x: 100 -> normalizedX/Y: 70
   appliedTo: 'transverseBaseXY'
 }
 
@@ -10,7 +13,12 @@ const shiftReducer = (state = shiftReducerInitState, action) => {
   switch (action.type) {
     case "SHIFT_MODIFIER_CHANGED":
       {
-        return Object.assign({}, state, action.payload);
+        // calc normalized x (joystick x * normalizer)
+        let normalizedX = MU.normalize(action.payload.x, state.normalizer),
+            // calc normalized y  (joystick y * normalizer)
+            normalizedY = MU.normalize(action.payload.y, state.normalizer);
+        
+        return Object.assign({}, state, action.payload, { normalizedX, normalizedY });
       }
   }
   return state;
