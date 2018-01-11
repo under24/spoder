cf6848ceb253d4b12d2c445c2193e002f04dd23d
Constantine <50499405@mail.ru>
2018-01-11 20:37:24 +0300

    [Minor] Temporarily disabled redux dev tools

--- public/assets/redux/initRedux.js
+++ public/assets/redux/initRedux.js
@@ -55,7 +55,7 @@ const reducers = combineReducers({
 
 const store = createStore(
   reducers, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const ReduxMixin = PolymerRedux(store);
