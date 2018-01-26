eadfc73abf0a1d8bde2d71d921d64a3697da7a4c
Constantine <50499405@mail.ru>
2018-01-26 23:18:41 +0300

    [Minor] Syntax improvement

--- public/assets/utils/reduxUtils.js
+++ public/assets/utils/reduxUtils.js
@@ -16,8 +16,7 @@ let RU = {
         ub, 
         denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
 
-    if (denom == 0) 
-      return null;
+    if (denom == 0) return null;
     
     ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denom;
     ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denom;
@@ -27,7 +26,7 @@ let RU = {
         y: y1 + ua * (y2 - y1),
         // seg1: ua >= 0 && ua <= 1,
         // seg2: ub >= 0 && ub <= 1
-    }
+    };
   },
   getRotatedCoords(baseCenterCoords, rotationAngle, coords, side) {    
     // use flipped values for the right side
@@ -42,6 +41,6 @@ let RU = {
     return {
       x: side === 'right' ? MU.flipNumber(rotatedCoords.x) : rotatedCoords.x,
       y: rotatedCoords.y
-    }
+    };
   }
 }
