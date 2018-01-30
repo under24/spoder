a6025c6a4393643ff052c90c9162e5c4bf567c8e
Constantine <50499405@mail.ru>
2018-01-30 14:29:55 +0300

    Removed ReduxUtils (RU) from utils and moved its methods to MathUtils (MU)

--- public/assets/utils/MathUtils.js
+++ public/assets/utils/MathUtils.js
@@ -50,6 +50,45 @@ let MU = {
   // used for mapping joystick values
   normalize(value, normalizer) {
     return this.roundNumber(value * normalizer, 2);
+  },
+  getBaseCenter(coords) {
+    let x1 = coords[1].transverseBaseX,
+        y1 = coords[1].transverseBaseY,
+        x2 = coords[6].transverseBaseX,
+        y2 = coords[6].transverseBaseY,
+        x3 = coords[2].transverseBaseX,
+        y3 = coords[2].transverseBaseY,
+        x4 = coords[5].transverseBaseX,
+        y4 = coords[5].transverseBaseY;
+    
+﻿    let ua, 
+        ub, 
+        denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+
+    if (denom == 0) return null;
+    
+    ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denom;
+    ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denom;
+    
+    return {
+        x: x1 + ua * (x2 - x1),
+        y: y1 + ua * (y2 - y1),
+        // seg1: ua >= 0 && ua <= 1,
+        // seg2: ub >= 0 && ub <= 1
+    };
+  },
+  getRotatedCoords(baseCenterCoords, rotationAngle, coords) {
+    var dx = coords.transverseBaseX - baseCenterCoords.x,
+        dy = coords.transverseBaseY - baseCenterCoords.y,
+        distance = MU.getDistance(dx, dy),
+        angle = MU.getAngle(dy, dx);
+        
+    var rotatedCoords = MU.getCoordsFromDistanceAndAngle(baseCenterCoords.x, baseCenterCoords.y, angle + rotationAngle, distance);
+
+    return {
+      x: rotatedCoords.x,
+      y: rotatedCoords.y
+    };
   }
   // interceptOnCircleOrig(p1, p2, c, r) {
   //   //p1 is the first line point
