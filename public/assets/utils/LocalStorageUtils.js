6150ec1cd40b46ebf9e99345b39e0dd6e2c2a88b
Constantine <50499405@mail.ru>
2018-02-02 23:22:27 +0300

    Added methods for LSU to get, save and remove items from local storage

--- public/assets/utils/LocalStorageUtils.js
+++ public/assets/utils/LocalStorageUtils.js
@@ -2,5 +2,27 @@
 
 // LSU == LocalStorageUtils
 var LSU = {
-  
+  remove(item) {
+    localStorage.removeItem(item);
+  },
+  get(item) {
+    return localStorage.getItem(item);
+  },
+  save(key, payload) {
+    // get settings from storage (if there are any)
+    var localSettings = localStorage.getItem(key);
+    
+    // null (no such values in local storage)
+    if (!localSettings) {
+      localSettings = payload;
+    }
+    // something is present in the storage -> parse it
+    else {
+      // merge payload with storage settings
+      localSettings = Object.assign({}, JSON.parse(localSettings), payload);
+    }
+
+    // convert settings to json and save back to local storage
+    localStorage.setItem(key, JSON.stringify(localSettings));
+  }
 };
