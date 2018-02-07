8926bfa6c981d64d935c3627d30c655887608bd7
Constantine <50499405@mail.ru>
2018-02-07 23:55:05 +0300

    Added preliminary code for movement correction

--- public/assets/utils/MathUtils.js
+++ public/assets/utils/MathUtils.js
@@ -89,7 +89,7 @@ var MU = {
       x: rotatedCoords.x,
       y: rotatedCoords.y
     };
-  }
+  },
   // interceptOnCircleOrig(p1, p2, c, r) {
   //   //p1 is the first line point
   //   //p2 is the second line point
@@ -115,30 +115,31 @@ var MU = {
   //       return [i1, i2];
   //   }
   // },
-  // interceptOnCircle(p1, p2, c, r) {
-  //   //p1 is the first line point
-  //   //p2 is the second line point
-  //   //c is the circle's center
-  //   //r is the circle's radius
-  // 
-  //   var p3 = {x:p1.x - c.x, y:p1.y - c.y}; //shifted line points
-  //   var p4 = {x:p2.x - c.x, y:p2.y - c.y};
-  // 
-  //   var m = (p4.y - p3.y) / (p4.x - p3.x); //slope of the line
-  //   debugger;
-  //   var b = p3.y - m * p3.x; //y-intercept of line
-  // 
-  //   var underRadical = Math.pow(r,2)*Math.pow(m,2) + Math.pow(r,2) - Math.pow(b,2); //the value under the square root sign
-  // 
-  //   if (underRadical < 0) {
-  //       //line completely missed
-  //       return false;
-  //   } else {
-  //       var t1 = (-m*b + Math.sqrt(underRadical))/(Math.pow(m,2) + 1); //one of the intercept x's
-  //       var t2 = (-m*b - Math.sqrt(underRadical))/(Math.pow(m,2) + 1); //other intercept's x
-  //       // var i1 = {x:t1+c.x, y:m*t1+b+c.y}; //intercept point 1
-  //       var i2 = {x:t2+c.x, y:m*t2+b+c.y}; //intercept point 2
-  //       return i2;
-  //   }
-  // }
+  // p1 is the first line point
+  // p2 is the second line point
+  // c is the circle's center
+  // r is the circle's radius
+  interceptOnCircle(p1, p2, c, r) {
+    var p3 = { x: p1.x - c.x, y: p1.y - c.y }; // shifted line points
+    var p4 = { x: p2.x - c.x, y: p2.y - c.y };
+  
+    var m = (p4.y - p3.y) / (p4.x - p3.x); // slope of the line
+    var b = p3.y - m * p3.x; // y-intercept of line
+  
+    var underRadical = Math.pow(r,2) * Math.pow(m,2) + Math.pow(r,2) - Math.pow(b,2); // the value under the square root sign
+  
+    // line completely missed
+    if (underRadical < 0) 
+      return null;
+    // line is withit the circle
+    else {
+      var t1 = (-m * b + Math.sqrt(underRadical)) / (Math.pow(m,2) + 1), // one of the intercept x's
+          t2 = (-m * b - Math.sqrt(underRadical)) / (Math.pow(m,2) + 1); // other intercept's x
+          
+      // var i1 = { x: t1 + c.x, y: m * t1 + b + c.y }; // intercept point 1
+      var i2 = { x: t2 + c.x, y: m * t2 + b + c.y }; // intercept point 2
+      
+      return i2;
+    }
+  }
 };
