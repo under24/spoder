fd789af64f6ba17134e2bba908f4d075ce6b1c00
Constantine <50499405@mail.ru>
2018-02-14 22:35:52 +0300

    [Minor] Temporarily commented out preliminary code

--- public/assets/redux/middleware/ModifierLogicReducer.js
+++ public/assets/redux/middleware/ModifierLogicReducer.js
@@ -28,19 +28,19 @@ class ModifierLogicReducer extends LogicReducer {
     this.flushProps(['level']);
   }
 
-  processRotationModifier(action) {
-    this.syncProps(['rotation']);
-    
-    // validation
-    if (this.rotation.x === action.payload.x) throw new Error('same values');
-    
-    // calc normalized x (joystick value * normalizer)
-    action.payload.normalizedX = MU.normalize(action.payload.x, this.rotation.normalizer);
-    action.path = this.getPropPath('rotation');
-    
-    this.flushProps(['rotation']);
-    
-    return action;
-  }
+  // processRotationModifier(action) {
+  //   this.syncProps(['rotation']);
+  //   
+  //   // validation
+  //   if (this.rotation.x === action.payload.x) throw new Error('same values');
+  //   
+  //   // calc normalized x (joystick value * normalizer)
+  //   action.payload.normalizedX = MU.normalize(action.payload.x, this.rotation.normalizer);
+  //   action.path = this.getPropPath('rotation');
+  //   
+  //   this.flushProps(['rotation']);
+  //   
+  //   return action;
+  // }
 
 }
