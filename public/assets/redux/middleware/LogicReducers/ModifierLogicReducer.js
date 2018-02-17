0ee3ae7f9a617c81dd51e8a100dd6e8d373c4470
Constantine <50499405@mail.ru>
2018-02-17 18:00:27 +0300

    Refactored with the new syntax and functionality

--- public/assets/redux/middleware/LogicReducers/ModifierLogicReducer.js
+++ public/assets/redux/middleware/LogicReducers/ModifierLogicReducer.js
@@ -5,75 +5,59 @@ class ModifierLogicReducer extends LogicReducer {
   constructor(store) {
     super(store);
     
-    this.properties = {
-      level: 'modifiers.level',
-      rotation: 'modifiers.rotation',
-      shift: 'modifiers.shift',
-      // tilt: 'modifiers.tilt'
-    };
-    
-    this.actions = [
+    this.actionTypes = [
       'processLevelModifier(LEVEL_MODIFIER_CHANGED)',
       'processRotationModifier(ROTATION_MODIFIER_CHANGED)',
       'processShiftModifier(SHIFT_MODIFIER_CHANGED)'
     ];
   }
-  
-  processAction(action, newState) {
-    switch (action.type) {
-      case "LEVEL_MODIFIER_CHANGED":
-        this.processLevelModifier(action.payload, newState); break;
-      case "ROTATION_MODIFIER_CHANGED":
-        this.processRotationModifier(action.payload, newState); break;
-      case "SHIFT_MODIFIER_CHANGED":
-        this.processShiftModifier(action.payload, newState); break;
-    }
-  }
 
-  processLevelModifier(payload, newState) {
-    this.syncProps(['level']);
+  processLevelModifier(payload) {
+    var oldLevel = this.resolvePath('modifiers.level');
     
     // validation. check if the payload and state values are the same
-    if (this.level.y === payload.y) throw new Error('same level modifier values');
+    if (oldLevel.y === payload.y) throw new Error('same level modifier values');
     
     // calc normalized y (joystick value * normalizer)
-    let normalizedY = MU.normalize(payload.y, this.level.normalizer);
+    var normalizedY = MU.normalize(payload.y, oldLevel.normalizer);
     
     // create new state branch 'modifiers.level' which is going to be merged with the current state
-    newState['modifiers.level'] = Object.assign({}, this.level, payload, { normalizedY });
-    
-    this.flushProps(['level']);
+    var newLevel = Object.assign({}, oldLevel, payload, { normalizedY });
+      
+    return { 'modifiers.level': newLevel };
   }
 
-  processRotationModifier(payload, newState) {
-    this.syncProps(['rotation']);
+  processRotationModifier(payload) {
+    var oldRotation = this.resolvePath('modifiers.rotation');
     
     // validation. check if the payload and state values are the same
-    if (this.rotation.x === payload.x) throw new Error('same rotation modifier values');
+    if (oldRotation.x === payload.x) throw new Error('same rotation modifier values');
     
     // calc normalized x (joystick value * normalizer)
-    let normalizedX = MU.normalize(payload.x, this.rotation.normalizer);
+    var normalizedX = MU.normalize(payload.x, oldRotation.normalizer);
     
-    newState['modifiers.rotation'] = Object.assign({}, this.rotation, payload, { normalizedX });
+    // create new state branch 'modifiers.rotation' which is going to be merged with the current state
+    var newRotation = Object.assign({}, oldRotation, payload, { normalizedX });
     
-    this.flushProps(['rotation']);
+    return { 'modifiers.rotation': newRotation };  
   }
   
-  processShiftModifier(payload, newState) {
-    this.syncProps(['shift']);
+  processShiftModifier(payload) {
+    var oldShift = this.resolvePath('modifiers.shift');
     
     // validation. check if the payload and state values are the same
-    if (this.shift.x === payload.x &&
-        this.shift.y === payload.y) throw new Error('same shift modifier values');
+    if (oldShift.x === payload.x &&
+        oldShift.y === payload.y) throw new Error('same shift modifier values');
         
     // calc normalized x (joystick x * normalizer)
-    let normalizedX = MU.normalize(payload.x, this.shift.normalizer),
+    var normalizedX = MU.normalize(payload.x, oldShift.normalizer),
         // calc normalized y  (joystick y * normalizer)
-        normalizedY = MU.normalize(payload.y, this.shift.normalizer);
+        normalizedY = MU.normalize(payload.y, oldShift.normalizer);
     
-    newState['modifiers.shift'] = Object.assign({}, this.shift, payload, { normalizedX, normalizedY });
+    // create new state branch 'modifiers.shift' which is going to be merged with the current state
+    var newShift = Object.assign({}, oldShift, payload, { normalizedX, normalizedY });
     
-    this.flushProps(['shift']);
+    return { 'modifiers.shift': newShift };
   }
 
 }
