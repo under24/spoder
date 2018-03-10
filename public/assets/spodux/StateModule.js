1a16349275b4be8bbf8ac19a113ba352a3527d84
Constantine <50499405@mail.ru>
2018-03-10 17:22:04 +0300

    [Minor] Commented out unused code

--- public/assets/spodux/StateModule.js
+++ public/assets/spodux/StateModule.js
@@ -12,45 +12,45 @@ class StateModule {
   }
   
   // [props...]
-  syncProps(props) {
-    // array is passed
-    if (props) {
-      for (let i = 0; i < props.length; i++) {
-        let propName = props[i],
-            propStatePath = this._getPropPath(propName),
-            propStateValue = this.resolveStatePath(propStatePath);
-        
-        this[propName] = propStateValue;
-      }
-    }
-    // no argument is passed
-    else {
-      for (let key in this.properties) {
-        let propName = key,
-            propStatePath = this._getPropPath(propName),
-            propStateValue = this.resolveStatePath(propStatePath);
-            
-        this[propName] = propStateValue;
-      }
-    }
-  }
+  // syncProps(props) {
+  //   // array is passed
+  //   if (props) {
+  //     for (let i = 0; i < props.length; i++) {
+  //       let propName = props[i],
+  //           propStatePath = this._getPropPath(propName),
+  //           propStateValue = this.resolveStatePath(propStatePath);
+  //       
+  //       this[propName] = propStateValue;
+  //     }
+  //   }
+  //   // no argument is passed
+  //   else {
+  //     for (let key in this.properties) {
+  //       let propName = key,
+  //           propStatePath = this._getPropPath(propName),
+  //           propStateValue = this.resolveStatePath(propStatePath);
+  //           
+  //       this[propName] = propStateValue;
+  //     }
+  //   }
+  // }
   
   // [props...]
-  flushProps(props) {
-    // argument is passed
-    if (props) {
-      for (let i = 0; i < props.length; i++) {
-        let propName = props[i];
-        delete this[propName];
-      }
-    }
-    // no argument is passed
-    else {
-      for (let propName in this.properties) {
-        delete this[propName]; // this[propName] = undefined;
-      }
-    }
-  }
+  // flushProps(props) {
+  //   // argument is passed
+  //   if (props) {
+  //     for (let i = 0; i < props.length; i++) {
+  //       let propName = props[i];
+  //       delete this[propName];
+  //     }
+  //   }
+  //   // no argument is passed
+  //   else {
+  //     for (let propName in this.properties) {
+  //       delete this[propName]; // this[propName] = undefined;
+  //     }
+  //   }
+  // }
   
   resolveStatePath(path) {
     var prop = this._getState();
@@ -65,18 +65,18 @@ class StateModule {
     return prop;
   }
   
-  getPropStateValue(propName) {
-    // validation
-    if (!this.properties[propName])
-      throw new Error('no such propname in properties');
-    
-    // get property state path
-    var propStatePath = this._getPropPath(propName),
-        // get property state value
-        propStateValue = this.resolveStatePath(propStatePath);
-        
-    return propStateValue;
-  }
+  // getPropStateValue(propName) {
+  //   // validation
+  //   if (!this.properties[propName])
+  //     throw new Error('no such propname in properties');
+  //   
+  //   // get property state path
+  //   var propStatePath = this._getPropPath(propName),
+  //       // get property state value
+  //       propStateValue = this.resolveStatePath(propStatePath);
+  //       
+  //   return propStateValue;
+  // }
   
   _getPropPath(propName) {
     // notify that there is no such prop declared
