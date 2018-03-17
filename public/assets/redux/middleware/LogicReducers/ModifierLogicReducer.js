143810c8ae1f5ebf929e34a09d0a0fdcb1b4a25c
Constantine <50499405@mail.ru>
2018-03-17 15:33:26 +0300

    [Minor] Syntax improvement

--- public/assets/redux/middleware/LogicReducers/ModifierLogicReducer.js
+++ public/assets/redux/middleware/LogicReducers/ModifierLogicReducer.js
@@ -24,7 +24,10 @@ class ModifierLogicReducer extends LogicReducer {
     var oldLevel = this.resolveStatePath('modifiers.level');
     
     // validation. check if the payload and state values are the same
-    if (oldLevel.y === payload.y) throw new Error('same level modifier values');
+    if (oldLevel.y === payload.y) {
+      console.warn('same level modifier values');
+      return;
+    }
     
     // calc normalized y (joystick y * normalizer)
     var normalizedY = MU.normalize(payload.y, oldLevel.normalizer);
@@ -39,7 +42,10 @@ class ModifierLogicReducer extends LogicReducer {
     var oldRotation = this.resolveStatePath('modifiers.rotation');
     
     // validation. check if the payload and state values are the same
-    if (oldRotation.x === payload.x) throw new Error('same rotation modifier values');
+    if (oldRotation.x === payload.x) {
+      console.warn('same rotation modifier values');
+      return;
+    }
     
     // calc normalized x (joystick x * normalizer)
     var normalizedX = MU.normalize(payload.x, oldRotation.normalizer);
@@ -54,14 +60,16 @@ class ModifierLogicReducer extends LogicReducer {
     var oldShift = this.resolveStatePath('modifiers.shift');
     
     // validation. check if the payload and state values are the same
-    if (oldShift.x === payload.x &&
-        oldShift.y === payload.y) throw new Error('same shift modifier values');
-        
+    if (oldShift.x === payload.x && oldShift.y === payload.y) {
+      console.warn('same shift modifier values');
+      return;
+    }
+    
     // calc normalized x (joystick x * normalizer)
     var normalizedX = MU.normalize(payload.x, oldShift.normalizer),
         // calc normalized y (joystick y * normalizer)
         normalizedY = MU.normalize(payload.y, oldShift.normalizer);
-    
+        
     // create new state branch 'modifiers.shift' which is going to be merged with the current state
     var newShift = Object.assign({}, oldShift, payload, { normalizedX, normalizedY });
     
@@ -72,14 +80,16 @@ class ModifierLogicReducer extends LogicReducer {
     var oldTilt = this.resolveStatePath('modifiers.tilt');
     
     // validation. check if the payload and state values are the same
-    if (oldTilt.x === payload.x &&
-        oldTilt.y === payload.y) throw new Error('same tilt modifier values');
-        
+    if (oldTilt.x === payload.x && oldTilt.y === payload.y) {
+      console.warn('same tilt modifier values');
+      return;
+    }
+    
     // calc normalized x (joystick x * normalizer)
     var normalizedX = MU.normalize(payload.x, oldTilt.normalizer),
         // calc normalized y (joystick y * normalizer)
         normalizedY = MU.normalize(payload.y, oldTilt.normalizer);
-    
+        
     // create new state branch 'modifiers.tilt' which is going to be merged with the current state
     var newTilt = Object.assign({}, oldTilt, payload, { normalizedX, normalizedY });
     
