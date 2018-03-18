02f2816c526407ee2de8ff8a42857742e32f8718
Constantine <50499405@mail.ru>
2018-03-18 15:01:33 +0300

    Removed <blueprint-generator> and replced its functionality with MovementIterationBlueprintCascadeModule.js

--- public/assets/redux/reducers/movement/iteration/blueprint.js
+++ public/assets/redux/reducers/movement/iteration/blueprint.js
@@ -4,13 +4,6 @@
   let movementIterationBlueprintReducerInitState = null;
 
   var movementIterationBlueprintReducer = (state = movementIterationBlueprintReducerInitState, action) => {
-    // switch (action.type) {
-    //   case "MOVEMENT_ITERATION_BLUEPRINT_CHANGED":
-    //     {
-    //       // action.payload is a blueprint which is create every time anew
-    //       return action.payload;
-    //     }
-    // }
     if ('movement.iteration.blueprint' in action) {
       var stateChange = action['movement.iteration.blueprint'];
       
