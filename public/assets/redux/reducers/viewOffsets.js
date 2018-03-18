870af550e39b6af03cb4880cc800afaee17e4f88
Constantine <50499405@mail.ru>
2018-03-18 21:27:28 +0300

    Moved viewOffsets reducer logic to viewOffsetsLogicReducer

--- public/assets/redux/reducers/viewOffsets.js
+++ public/assets/redux/reducers/viewOffsets.js
@@ -29,26 +29,6 @@
   };
 
   var viewOffsetsReducer = (state = viewOffsetsReducerInitState, action) => {
-    // switch (action.type) {
-    //   case "SEQUENCE_SHIFTED_XY_BATCHED":
-    //     {
-    //       let newState = {};
-    //       
-    //       for (let legId in action.payload) {
-    //         
-    //         let payload = {};
-    //         
-    //         if (action.payload[legId].transverseBaseX)
-    //           payload.transverseX = state[legId].transverseX + action.payload[legId].transverseBaseX;
-    //         
-    //         if (action.payload[legId].transverseBaseY)
-    //           payload.transverseY = state[legId].transverseY + action.payload[legId].transverseBaseY;
-    //         
-    //         newState[legId] = Object.assign({}, state[legId], payload);
-    //       }
-    //       return Object.assign({}, state, newState);
-    //     }
-    // }
     if ('viewOffsets' in action) {
       var stateChange = action['viewOffsets'];
       
