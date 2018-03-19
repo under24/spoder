d0bbd7279c4c23f561993d1332751c3faa01e0f2
Constantine <50499405@mail.ru>
2018-03-19 23:30:23 +0300

    Moved turn joystick logic from reducer to middleware

--- public/assets/redux/reducers/movement/turnJoystick.js
+++ public/assets/redux/reducers/movement/turnJoystick.js
@@ -8,15 +8,6 @@
   };
 
   var movementTurnJoystickReducer = (state = movementTurnJoystickReducerInitState, action) => {
-    // switch (action.type) {
-    //   case "MOVEMENT_TURN_JOYSTICK_VALUES_CHANGED":
-    //     {
-    //       // calc normalized x (joystick value * normalizer)
-    //       let normalizedX = MU.normalize(action.payload.x, state.normalizer);
-    // 
-    //       return Object.assign({}, state, action.payload, { normalizedX });
-    //     }
-    // }
     if ('movement.turnJoystick' in action) {
       var stateChange = action['movement.turnJoystick'];
       
