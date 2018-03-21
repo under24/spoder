94b2ea9a5b1e5dbe64cbd1a08d9b405f66b4f7ab
Constantine <50499405@mail.ru>
2018-03-21 23:58:16 +0300

    Syntax improvement + added syncState emission when a new client connects to the server

--- server/modules/socket.js
+++ server/modules/socket.js
@@ -1,17 +1,24 @@
 'use strict';
 
 module.exports = (shared, server) => {
+  
   // initialize socket.io
   var io = require('socket.io')(server);
+  
   // when a new client is connected
   io.on('connection', function (client) {
     console.log('client connected');
     
-    client.on('action', function() {
+    // send the current logic state to every new client
+    client.emit('syncState', shared.resolve('store').getState());
+    
+    // client dispatched an action
+    client.on('action', () => {
       console.log('new action received');
     });
   });
   
   // share socket.io between modules
   shared.register('io', io);
+  
 };
