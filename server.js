12cc8a6a2f34de8da8a096bec73f9abc3fb40d16
Constantine <50499405@mail.ru>
2018-04-01 00:06:10 +0300

    Refactored server.js to keep the structure clean

--- server.js
+++ server.js
@@ -1,110 +1,24 @@
 'use strict';
 
-const five = require('johnny-five');
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-// const fs = require('fs');
+// webserver
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app);
+    
+// init object for storing shared objects
+var shared = require('./server/utils/Shared')();
+
+require('./server/modules/socket')(shared, server);
+require('./server/modules/store')(shared);
+// require('./server/modules/joystick')(shared);
+// require('./server/modules/arduino')(shared);
+// require('./server/modules/webrtc')(shared);
 
 app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-
-// var board = new five.Board({
-//   port: "/dev/cu.SPODER-SPPDev"
-// });
-// 
-// board.on('ready', function() {
-//   console.log('Arduino board is ready.');
-//   
-//   // board servo
-//   var a = new five.Servo(9);
-//   var ab = new five.Servo(10);
-//   
-//   // expansion board
-//   var a = new five.Servo({
-//     address: 0x40,
-//     controller: "PCA9685",
-//     pin: 8,
-//     invert: true,
-//     throttle: true
-//   });
-//   
-//   var b = new five.Servo({
-//     address: 0x40,
-//     controller: "PCA9685",
-//     range: [0, 180],
-//     pin: 15,
-//     throttle: true
-//   });
-//   // 
-//   var c = new five.Servo({
-//     address: 0x40,
-//     controller: "PCA9685",
-//     range: [0, 180],
-//     pin: 5,
-//     throttle: true
-//   });
-//   // 
-//   var d = new five.Servo({
-//     address: 0x40,
-//     controller: "PCA9685",
-//     range: [0, 180],
-//     pin: 11,
-//     throttle: true
-//   });
-//   
-//   var e = new five.Servo({
-//     address: 0x40,
-//     controller: "PCA9685",
-//     range: [0, 180],
-//     pin: 12,
-//     throttle: true
-//   });
-//   
-//   var f = new five.Servo({
-//     address: 0x40,
-//     controller: "PCA9685",
-//     range: [0, 180],
-//     pin: 0,
-//     throttle: true
-//   });
-//   
-//   var g = new five.Servo({
-//     address: 0x40,
-//     controller: "PCA9685",
-//     range: [0, 180],
-//     pin: 3,
-//     throttle: true
-//   });
-// 
-//   io.on('connection', function(client) {
-//     client.on('join', function(handshake) {
-//       console.log(handshake);
-//     });
-//         
-//     client.on('femur-servo-angle-changed', (data) => {
-//       console.log(data.value);
-//       a.to(data.value);
-//       ab.to(data.value);
-//       b.to(data.value);
-//       c.to(data.value);
-//       d.to(data.value);
-//       e.to(data.value);
-//       f.to(data.value);
-//       g.to(data.value);
-//     });
-//     
-//   });
-//   
-//   // this.repl.inject({
-//   //   
-//   // });
-// });
-
-const port = process.env.PORT || 3000;
+var port = 3000;
 server.listen(port);
 console.log(`Server is running at localhost:${port}`);
