7e9fda22bd6149b14ead98030ae5dba15eefc46b
Constantine <50499405@mail.ru>
2018-04-01 23:36:58 +0300

    Moved joystick handling to middleware

--- public/assets/js/SocketListenerStateModule.js
+++ public/assets/js/SocketListenerStateModule.js
@@ -13,10 +13,6 @@ class SocketListenerStateModule extends StateModule {
     socket.on('connect', this.handleSocketConnected.bind(this));
     // socket disconnected
     socket.on('disconnect', this.handleSocketDisconnected.bind(this));
-    // joystick right stick
-    // socket.on('right-analog-stick-moved', this.handleRightAnalogStickMoved.bind(this));
-    // joystick left stick
-    // socket.on('left-analog-stick-moved', this.handleLeftAnalogStickMoved.bind(this));
     // logic state changed
     socket.on('state-change', this.handleStateChange.bind(this));
     // pull the logic state from the server and merge it with the client state
@@ -30,26 +26,6 @@ class SocketListenerStateModule extends StateModule {
   handleSocketDisconnected() {
     this._dispatch({ type: "SOCKET_STATUS_CHANGED", payload: { connected: false } });
   }
-  
-  // handleRightAnalogStickMoved(data) {
-  //   SocketListenerStateModule.store.dispatch({
-  //     type: "MOVEMENT_TURN_JOYSTICK_VALUES_CHANGED",
-  //     payload: {
-  //       x: data.x,
-  //       y: data.y
-  //     }
-  //   });
-  // }
-  
-  // handleLeftAnalogStickMoved(data) {
-  //   SocketListenerStateModule.store.dispatch({
-  //     type: "MOVEMENT_DIRECTION_JOYSTICK_VALUES_CHANGED",
-  //     payload: {
-  //       x: data.x,
-  //       y: data.y
-  //     }
-  //   });
-  // }
   
   handleStateChange(stateChange) {
     this._dispatch(stateChange);
